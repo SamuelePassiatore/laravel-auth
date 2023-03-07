f7dd226023a1e5b3ab21d8bf3eac6ef67b7face2
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-07 15:48:47 +0100

    created home controller in guest folder & return to view guest.home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/admin', function () {
     return view('admin.home');
