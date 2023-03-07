aac2738abefd5f82998e3fb1475f509f626fcee4
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-07 17:22:12 +0100

    created file projects php in config with all data (not completed)

---
+++ config/projects.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    [
+        "title" => "laravel-dc-comics",
+        "description" => "lorem ipsum",
+        "image" => "",
+        "slug" => "",
+        "url" => "",
+    ],
+    [
+        "title" => "",
+        "description" => "",
+        "image" => "",
+        "slug" => "",
+        "url" => "",
+    ],
+    [
+        "title" => "",
+        "description" => "",
+        "image" => "",
+        "slug" => "",
+        "url" => "",
+    ],
+    [
+        "title" => "",
+        "description" => "",
+        "image" => "",
+        "slug" => "",
+        "url" => "",
+    ],
+    [
+        "title" => "",
+        "description" => "",
+        "image" => "",
+        "slug" => "",
+        "url" => "",
+    ],
+];
