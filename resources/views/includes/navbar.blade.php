789f4240f3d16db43d7406908b7f0f35ffece6a6
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-07 18:42:09 +0100

    added route is class active on current page link

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -16,7 +16,12 @@
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                    <a class="nav-link @if (Route::is('admin.home')) active @endif"
+                        href="{{ url('/') }}">{{ __('Home') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link @if (Route::is('admin.projects.index')) active @endif"
+                        href="{{ route('admin.projects.index') }}">{{ __('Projects') }}</a>
                 </li>
             </ul>
 
