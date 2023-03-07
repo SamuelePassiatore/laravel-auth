fea8bc7add6cb7749e9e7abb6b2137b8fec646b8
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-07 17:09:20 +0100

    added migration table with all elements

--- database/migrations/2023_03_07_151543_create_projects_table.php
+++ database/migrations/2023_03_07_151543_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100)->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('slug');
+            $table->string('url')->nullable();
             $table->timestamps();
         });
     }
