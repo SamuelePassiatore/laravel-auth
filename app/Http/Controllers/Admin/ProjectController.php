c2420baf819f7680b98c16435c211737c27eebcd
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-07 18:54:34 +0100

    order element by updated at at index and added crud show return view

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::orderBy('updated_at', 'DESC')->get();
         return view('admin.projects.index', compact('projects'));
     }
 
@@ -38,7 +38,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+        return view('admin.projects.show', compact('project'));
     }
 
     /**
