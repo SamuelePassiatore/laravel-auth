1e2863d31c434763afddec6372dc6d91558cc063
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-08 16:06:42 +0100

    added extends layout and header to create blade

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Crea un progetto')
+
+@section('content')
+
+    <header class="my-4 d-flex justify-content-between align-items-center">
+        <h1>Crea progetto</h1>
+    </header>
+
+
+@endsection
+
+@section('scripts')
+
+@endsection
