2359c28860535df233f8e7cc80c4a3f4d7f3c931
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-08 16:09:14 +0100

    added return in function create of project controller

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('title', 'Crea un progetto')
+@section('title', 'Nuovo progetto')
 
 @section('content')
 
     <header class="my-4 d-flex justify-content-between align-items-center">
-        <h1>Crea progetto</h1>
+        <h1>Nuovo progetto</h1>
     </header>
 
 
